e247473fbe9b76a505ab9e00bb9c12b72229e5d8
chauquocan <chauquocan2090@gmail.com>
2021-09-15 08:07:41 +0700

    commit base project with packges

---
+++ src/main/java/com/quocan/quocan_springboot_api/QuocanSpringbootApiApplication.java
@@ -0,0 +1,13 @@
+package com.quocan.quocan_springboot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuocanSpringbootApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuocanSpringbootApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quocan/quocan_springboot_api/QuocanSpringbootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quocan.quocan_springboot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuocanSpringbootApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
