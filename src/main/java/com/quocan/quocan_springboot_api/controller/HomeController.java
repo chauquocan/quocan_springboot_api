cb29aa8806ac6d5771ac0b7d9a1acf43b05c9969
chauquocan <chauquocan2090@gmail.com>
2021-09-15 08:45:43 +0700

    commit add classes,interface,config database,home controller

--- src/main/java/com/quocan/quocan_springboot_api/controller/HomeController.java
+++ src/main/java/com/quocan/quocan_springboot_api/controller/HomeController.java
@@ -1,9 +1,11 @@
 package com.quocan.quocan_springboot_api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    @GetMapping("/")
     public String HomePage(){
         return "Chau Quoc An - 18084851";
     }
